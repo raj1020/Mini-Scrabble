cf42e024d8d27479e46fed89f6cc0cdce035719d
raj1020 <raj.1020@hotmail.com>
2019-10-06 17:24:13 +1100

    completed the coding part

--- scrabble.rb
+++ scrabble.rb
@@ -16,11 +16,59 @@ E.g. word_ranking('reshow the shower') -> 'reshow'
 =end
 
 def word_ranking(str)
-	#Your code here
+    
+    alnum={ "a"=> 1, 
+            "b"=> 2,
+            "c"=> 3,
+            "d"=> 4,
+            "e"=> 5,
+            "f"=> 6,
+            "g"=> 7,
+            "h"=> 8,
+            "i"=> 9,
+            "j"=> 10,
+            "k"=> 11,
+            "l"=> 12,
+            "m"=> 13,
+            "n"=> 14,
+            "o"=> 15,
+            "p"=> 16,
+            "q"=> 17,
+            "r"=> 18,
+            "s"=> 19,
+            "t"=> 20,
+            "u"=> 21,
+            "v"=> 22,
+            "w"=> 23,
+            "x"=> 24,
+            "y"=> 25,
+            "z"=> 26
+        }
+        total=[]
 
-
+    str=str.downcase
+    strsplit=str.split(" ")
+        
+    for i in strsplit
+        strstrsplit=i.split("")
+        tot=0
+        for item in strstrsplit
+            tot+=alnum[item]
+            
+        end
+        total.push(tot)
+    end
+    
+    index1= total.index(total.max)
+    if str.length==0
+        return ''
+    else
+        return strsplit[index1]
+    end
+       
 
 end
 
 p word_ranking('hi there')
 p word_ranking('reshow the shower')
+p word_ranking('')
